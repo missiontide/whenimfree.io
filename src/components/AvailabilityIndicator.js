fbb838c6a7ab4092fbb28283d474457d1e112138
missiontide <patcoronel@missiontide.com>
2022-05-26 00:57:40 -0700

    reinstalled react-week-calendar

--- src/components/AvailabilityIndicator.js
+++ src/components/AvailabilityIndicator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import moment from "moment";
 import WeekCalendar from 'react-week-calendar';
 import 'react-week-calendar/dist/style.css';
 
@@ -15,7 +16,9 @@ class AvailabilityIndicator extends React.Component {
         return (
             <div>
                 <WeekCalendar
-                    scaleUnit={60}
+                    startTime={moment({h:8, m:0})}
+                    endTime={moment({h:21, m:0})}
+                    scaleUnit={30}
                     scaleFormat="H:mm"
                     dayFormat="ddd MMM DD"
                 />
