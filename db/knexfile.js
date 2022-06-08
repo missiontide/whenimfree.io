9e340556e943ddf450a9510a9459c806b27e5cd4
missiontide <patcoronel@missiontide.com>
2022-06-08 08:25:31 -0700

    adjusting knexfile

--- db/knexfile.js
+++ db/knexfile.js
@@ -21,35 +21,33 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+      client: 'postgresql',
+      connection: {
+          connectionString: process.env.DATABASE_URI,
+          ssl: { rejectUnauthorized: false }
+      },
+      pool: {
+          min: 2,
+          max: 10
+      },
+      migrations: {
+          tableName: 'knex_migrations'
+      },
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+      client: 'postgresql',
+      connection: {
+          connectionString: process.env.DATABASE_URI,
+          ssl: { rejectUnauthorized: false }
+      },
+      pool: {
+          min: 2,
+          max: 10
+      },
+      migrations: {
+          tableName: 'knex_migrations'
+      },
   }
 
 };
