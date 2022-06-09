a43c6803aa977a5a6e930d741005ae8de3c31a46
missiontide <patcoronel@missiontide.com>
2022-06-08 23:23:59 -0700

    Error handling on CreateScheduler w/ Toasts

--- components/CreateScheduler.js
+++ components/CreateScheduler.js
@@ -4,7 +4,7 @@ import EventNameInput from "./EventNameInput";
 import DayPickerCalendar from "./DayPickerCalendar"
 import TimeSelector from "./TimeSelector";
 import SubmitButton from "./SubmitButton";
-import { ProgressBar } from "react-bootstrap";
+import { ProgressBar, Toast, ToastContainer } from "react-bootstrap";
 
 import cuid from "cuid";
 import { parse } from "date-fns";
@@ -16,8 +16,44 @@ function CreateScheduler() {
     const [endTime, setEndTime] = useState("5:00 PM");
     const [loading, setLoading] = useState(false);
 
+    /*
+    ERROR HANDLING
+     */
+    const [errors, setErrors] = useState([])
+    function validateInputs() {
+        const newErrors = []
+        if (eventName === "") {
+            newErrors.push(<><b>Event name</b> cannot be blank</>)
+        }
+        if (eventName.length > 255) {
+            newErrors.push(<><b>Event name</b> be more than 255 characters</>)
+        }
+        if (days.length === 0) {
+            newErrors.push(<>Select at least <b>1 day</b></>)
+        }
+        if (startTime === endTime && startTime === "12:00 AM") {
+
+        } else {
+            if (parse(startTime, "p", new Date()) >= parse(endTime, "p", new Date())) {
+                newErrors.push(<><b>First time must be earlier.</b></>)
+            }
+        }
+
+        setErrors(newErrors)
+
+        return newErrors.length;
+    }
+
+    function removeError(errorJsx) {
+        const newErrors = errors.slice();
+        newErrors.splice(newErrors.findIndex(jsx => jsx === errorJsx), 1)
+        setErrors(newErrors)
+    }
+
     // create the scheduler and redirect user to the Availability Page
     function handleSubmit() {
+        if (validateInputs() > 0) { return } // if there are any errors, do not continue
+
         setLoading(true);
         fetch('/api/create-scheduler', {
             method: 'POST',
@@ -52,6 +88,20 @@ function CreateScheduler() {
                     </div>
                 </div>)
             }
+            <ToastContainer position="top-end" className="p-3">
+                {errors.map((errorJsx, idx) => {
+                    return (
+                        <Toast onClose={() => removeError(errorJsx)} key={idx}>
+                            <Toast.Header>
+                                <img className="rounded me-2" alt="" />
+                                <strong className="me-auto">Error</strong>
+                            </Toast.Header>
+                            <Toast.Body>{errorJsx}</Toast.Body>
+                        </Toast>
+                        )
+                })}
+            </ToastContainer>
+
             <div>
                 <EventNameInput
                     eventName={eventName}
