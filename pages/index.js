84f0c13e140edccbb189fcbdb443d9a00206848f
missiontide <patcoronel@missiontide.com>
2022-06-11 22:39:53 -0700

    utilized next/Head

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import styles from '../styles/Home.module.css'
 import CreateScheduler from '../components/CreateScheduler'
 import logo from "../public/logo.png";
 import Image from "next/image";
+import Head from 'next/head'
 
 export default function Home() {
   return (
       <>
-          <title>whenimfree.io</title>
           <div className={styles.container}>
               <p className="signature">
                   made by <a href="https://www.missiontide.com" target="_blank" rel="noreferrer">@missiontide</a>
@@ -14,11 +14,14 @@ export default function Home() {
               <Image src={logo} className="App-logo" alt="logo" />
               <CreateScheduler />
           </div>
-          <meta property="og:url" content="https://whenimfree.io" />
-          <meta property="og:title" content="whenimfree.io - Get availability fast!" />
-          <meta property="og:description" content="Quickly find the best time for an event with friends!" />
-          <meta property="og:image" content="../public/website-social-card.png" />
-          <meta property="og:type" content="website" />
+          <Head>
+              <title>whenimfree.io</title>
+              <meta property="og:url" content="https://whenimfree.io" />
+              <meta property="og:title" content="whenimfree.io - Get availability fast!" />
+              <meta property="og:description" content="Quickly find the best time for an event with friends!" />
+              <meta property="og:image" content="../public/social-media-card.png" />
+              <meta property="og:type" content="website" />
+          </Head>
       </>
 
   )
