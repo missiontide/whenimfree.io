0f4a4c2251f22d30ab06d74be1f3f5473187a1dc
missiontide <patcoronel@missiontide.com>
2022-06-12 23:55:38 -0700

    adding fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+
 import "@fontsource/montserrat";
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,7 +15,34 @@ import '../styles/AvailabilityList.css'
 import '../styles/phoneStyles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    /*
+    Enable Fathom Analytics on site
+     */
+    const router = useRouter();
+
+    useEffect(() => {
+        // Initialize Fathom when the app loads
+        // Example: yourdomain.com
+        //  - Do not include https://
+        //  - This must be an exact match of your domain.
+        //  - If you're using www. for your domain, make sure you include that here.
+        Fathom.load("GAATAYPY", {
+            includedDomains: ['whenimfree.io'],
+        });
+
+        function onRouteChangeComplete() {
+            Fathom.trackPageview();
+        }
+        // Record a pageview when route changes
+        router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+        // Unassign event listener
+        return () => {
+            router.events.off('routeChangeComplete', onRouteChangeComplete);
+        };
+    }, []);
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
